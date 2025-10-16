7afe87db48a5d9f525de2f754f0939a20822eec8
egastelumm <egastelum0375@gmail.com>
2025-10-16 16:00:41 -0700

    Updated the guide and top_dff.sv, included photo of waveforms

--- top_dff.sv
+++ top_dff.sv
@@ -1,10 +1,6 @@
 /* 
 By Edgar G.
-
 I created this top file to practice writing verification, learn QuestaSim, and test the dff.
-I was able to visually confirm the flip-flop works by generating waveforms. However, the flip-flop
-is a simple design, and assert statements would be better for more complex designs in the future.
-
 */
 
 `timescale 1ns/1ns // timescale for simulator, 1 ns as time unit, 1ns precision
@@ -13,6 +9,8 @@ module top_dff#(parameter int size = 8)();
 // adjust the size of the flip-flop and buses
 logic [size-1:0] d, q;
 logic clk, rst, en;
+
+logic [size] h; // for holding last random value
 
 // instantiate the module
 dff#(size) my_dff(.*);
@@ -48,6 +46,19 @@ end
 //creating random values to input into the flip-flop
 always @(negedge clk) begin
     d = $random();
+end
+
+always @(posedge clk) begin
+    h <= d;
+    #5 // short delay for setup/hold
+    unique case (1'b1)
+        //check flip-flop doesn't change when disabled
+        (rst && !en): assert(h != q) else $error("q changing when dff is disabled");
+        //checks flip-flop changes when enabled
+        (rst && en): assert(h == q) else $error("dff not accepting values");
+        //checks reset is working
+        default: assert(q == '0) else $error("dff is not resetting");
+    endcase
 end
 
 endmodule
