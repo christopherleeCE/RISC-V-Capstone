afd231a3b5fada395ccff33a6949759f151e7083
egastelumm <egastelum0375@gmail.com>
2025-10-27 16:29:34 -0700

    Added parameters to alu, created empty module for main processor.

--- Modules/alu.sv
+++ Modules/alu.sv
@@ -1,3 +1,10 @@
-module alu();
+module alu
+#(parameter data_width = 32) // width of input data - default 32 bits for RISC-V
+(
+    input logic [datawidth-1:0] operand_a, operand_b, // data input
+    input logic func3, func7, // control signals
+    output logic [datawidth-1:0] result // data output
+    
+);
 
 endmodule
---
+++ Modules/microprocessor.sv
@@ -0,0 +1,3 @@
+module microprocessor();
+
+endmodule
