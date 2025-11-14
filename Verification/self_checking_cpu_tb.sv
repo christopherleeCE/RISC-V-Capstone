6add67267d3a80c708fa8bc009af938dfd7495f8
egastelumm <egastelum0375@gmail.com>
2025-11-14 13:46:00 -0700

    Successful run of ADDI, LW, and SW. Need to fix ADD and SUB. Extended AY's program to test for ADD/SUB.

--- Verification/self_checking_cpu_tb.sv
+++ Verification/self_checking_cpu_tb.sv
@@ -1,22 +1,20 @@
 /*
 TESTING: TBD
-SUCCESSFUL TESTS: ADDI, LW, SW
-
-- This testbench has been a lot of trial and error, but I'm very happy with it so far. It should hopefully
-not be too hard to expand to other instruction types. Feel free to run it and let me know if you have any
-questions.
+FAILED TESTS: ADD, SUB
+SUCCESSFUL TESTS: ADDI/NOP, LW, SW
 
 - Load up a program in the instruction memory along with any contents in the data memory. Please note that you
 have to adjust the constants for the testbench to fully cover all instructions and data, or you may get a
 false error.
 
-- Edgar
+- Edgar G.
 
 */
 
 `timescale 1ns/1ns
 
 module self_checking_cpu_tb ();
+//PARAMETERS/BUSES----------------------------------------------------------------------------------------------------------
 //constants
 parameter int CLOCK_PERIOD = 20;
 parameter int NUM_INSTRUCTIONS = 32;
@@ -44,6 +42,7 @@ logic [31:0] data_mem_addr, data_mem_addr_v;
 logic [31:0] result, result_v;
 
 
+//DUT---------------------------------------------------------------------------------------------------------------------
 //instantiate the CPU
 riscv_cpu cpu_dut(.clk(clk), .rst(rst), .ohalt(ohalt));
 
@@ -133,11 +132,23 @@ end
 always_comb begin
     opcode = mem_wb[6:0]; //identify the instruction during write back
 
-    if (opcode == 7'b0010011) begin //----- I-TYPE ---------------------------------------
+    if (opcode == 7'b0110011) begin //----R-TYPE-------------------------------------------------
+
+        {func7, rs2, rs1, func3, rd} = mem_wb[31:7]; //breaking the instruction into components
+        if(func3 == 3'b000 && func7 == 7'b0000000) begin //----ADD------------------------------
+            result = cpu_dut.my_reg_file.regs_out[rs1] + cpu_dut.my_reg_file.regs_out[rs2]; //add
+
+        end else if (func3 == 3'b000 && func7 == 7'b0100000) begin //----SUB---------------------------
+            result = cpu_dut.my_reg_file.regs_out[rs1] - cpu_dut.my_reg_file.regs_out[rs2]; //subtract
+
+        end
+
+    end else if (opcode == 7'b0010011) begin //----- I-TYPE -------------------------------------
 
         {imm_i, rs1, func3, rd} = mem_wb[31:7]; //breaking the instruction into components
         if(func3 == 3'b000) begin //-------ADDI-------------------------------
-            result = cpu_dut.my_reg_file.regs_out[rs1] + {{20{imm_i[11]}}, imm_i};
+            result = cpu_dut.my_reg_file.regs_out[rs1] + {{20{imm_i[11]}}, imm_i}; //sign extend and add
+
         end
 
     end else if (opcode == 7'b0000011) begin //----I-TYPE----------------------------------
@@ -146,6 +157,7 @@ always_comb begin
         if(func3 == 3'b010) begin //----LW------------------------------------
             data_mem_addr = cpu_dut.my_reg_file.regs_out[rs1] + {20'b0, imm_i}; //calculate address
             result = data_memory[data_mem_addr[31:2]]; //load in word
+
         end
 
     end else if (opcode == 7'b0100011) begin //------S-TYPE----------------------------------
@@ -157,6 +169,10 @@ always_comb begin
             data_memory[data_mem_addr[31:2]] <= result; // write to data memory of testbench
 
         end
+
+    end else begin
+
+        result = 32'hdeadbeef; //placeholder value, instruction ignored anyways in verification
 
     end
 end
@@ -172,7 +188,8 @@ always @(negedge clk) begin
 
     end else if(instruction_failure == 1) begin //INSTRUCTION FAILURE----------------------------------
         $display("\nWARNING: Mismatch between model and CPU. Pausing verification.");
-        $stop(); //pauses verification if CPU outputs halt signal
+        $display("\tPlease check for data hazards and issues in instruction datapath/control.");
+        $stop(); //pauses verification if an instruction has failed OR a data hazard has occured.
 
     end else if(post_wb == 32'hdeadbeef) begin //NO MORE INSTRUCTIONS ----------------------------------
 
@@ -190,8 +207,30 @@ always @(negedge clk) begin
     $display("Currently verifying: 0x%h", post_wb); //instruction in verification stage
 
     //NOTE: the variables used here contain "_v"; they are the results from the final stage
+    //NOTE: There is some repitatin
 
-    if (opcode_v == 7'b0010011) begin //----- I-TYPE ---------------------------------------
+    if (opcode_v == 7'b0110011) begin //--------R-TYPE----------------------------------------------
+
+        $display("\tR-Type: func7: 0b%b, rs2: 0b%b, rs1: 0b%b, func3: 0b%b, rd: 0b%b, opcode: 0b%b",
+        func7_v, rs2_v, rs1_v, func3_v, rd_v, opcode_v); //instruction info
+
+        if(func3_v == 3'b000 && func7_v == 7'b0000000) begin //----ADD------------------------------
+            $display("\tIdentified as ADD.");
+            assert(cpu_dut.my_reg_file.regs_out[rd_v] == result_v) $display("ADD successful.");
+            else instruction_failure = 1;//compare contents of model register with CPU register
+
+        end else if (func3_v == 3'b000 && func7_v == 7'b0100000) begin //----SUB---------------------------
+            $display("\tIdentified as SUB.");
+            assert(cpu_dut.my_reg_file.regs_out[rd_v] == result_v) $display("SUB successful.");
+            else instruction_failure = 1;//compare contents of model register with CPU register
+
+        end
+
+        $display("Contents of register x%d:\n\tCPU: 0x%h\n\tModel: 0x%h", rd_v, cpu_dut.my_reg_file.regs_out[rd_v],
+        result_v); //model vs cpu register
+
+
+    end else if (opcode_v == 7'b0010011) begin //-----I-TYPE ---------------------------------------
 
         $display("\tI-Type: imm: 0b%b, rs1: 0b%b, func3: 0b%b, rd: 0b%b, opcode: 0b%b", imm_i_v, rs1_v,
         func3_v, rd_v, opcode_v); //instruction info
@@ -202,13 +241,13 @@ always @(negedge clk) begin
             end else begin
                 $display("\tIdentified as ADDI.");
             end
-
             assert(cpu_dut.my_reg_file.regs_out[rd_v] == result_v) $display("ADDI/NOP successful.");
             else instruction_failure = 1;//compare contents of model register with CPU register
 
-            $display("Contents of register x%d:\n\tCPU: 0x%h\n\tModel: 0x%h", rd_v, cpu_dut.my_reg_file.regs_out[rd_v],
-            result_v); //model vs cpu
         end
+
+        $display("Contents of register x%d:\n\tCPU: 0x%h\n\tModel: 0x%h", rd_v, cpu_dut.my_reg_file.regs_out[rd_v],
+        result_v); //model vs cpu register
 
     end else if (opcode_v == 7'b0000011) begin //----I-TYPE----------------------------------
 
@@ -218,33 +257,35 @@ always @(negedge clk) begin
         if(func3_v == 3'b010) begin //----LW------------------------------------
             $display("\tIdentified as LW.");
             $display("Reading from data memory at address 0x%h.", data_mem_addr_v); //display addr
-
             assert(cpu_dut.my_reg_file.regs_out[rd_v] == result_v) $display("LW successful.");
             else instruction_failure = 1;//compare contents of model register with CPU register
 
-            $display("Contents of register x%d:\n\tCPU: 0x%h\n\tModel: 0x%h", rd_v, cpu_dut.my_reg_file.regs_out[rd_v],
-            result_v); //model vs cpu
         end
+
+        $display("Contents of register x%d:\n\tCPU: 0x%h\n\tModel: 0x%h", rd_v, cpu_dut.my_reg_file.regs_out[rd_v],
+        result_v); //model vs cpu registers
 
     end else if (opcode_v == 7'b0100011) begin //------S-TYPE----------------------------------
 
-        $display("\tS-Type: imm: 0b%b, rs2: 0b%b, rs1: 0b%b, rs2: 0b%b, opcode: 0b%b", imm_s_v, rs2_v, rs1_v,
+        $display("\tS-Type: imm: 0b%b, rs2: 0b%b, rs1: 0b%b, func3: 0b%b, opcode: 0b%b", imm_s_v, rs2_v, rs1_v,
         func3_v, opcode_v); //instruction info
 
         if(func3_v == 3'b010) begin //----SW-------------------------------------
             $display("\tIdentified as SW.");
             $display("Writing to data memory at address 0x%h.", data_mem_addr_v); //display addr
-
             assert(cpu_dut.data_mem.data_mem[data_mem_addr_v[31:2]] == result_v) $display("SW successful.");
             else instruction_failure = 1;//ensure source register was written to CPU memory
 
-            $display("Contents of data memory at 0x%h:\n\tCPU: 0x%h\n\tModel: 0x%h",
-            data_mem_addr_v, cpu_dut.data_mem.data_mem[data_mem_addr_v[31:2]], result_v); //compare memory contents
         end
 
+        $display("Contents of data memory at 0x%h:\n\tCPU: 0x%h\n\tModel: 0x%h",
+        data_mem_addr_v, cpu_dut.data_mem.data_mem[data_mem_addr_v[31:2]], result_v); //compare model vs CPU memory contents
+
     end else begin
+
             //UNKNOWN ------------------------------
         $display("WARNING: Instruction type not currently recognized by TB.");
+
     end
 
     //CPU REGISTER DUMP - comment/uncomment as needed
