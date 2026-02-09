8cb505dc554ff56007310d500c217b3b8db22f54
christopherleeCE <christopher.lee.engineer@outlook.com>
2026-02-08 22:11:56 -0700

    tweaks to rand test

--- Modules/temp.s
+++ Modules/temp.s
@@ -1,27 +1,112 @@
-li a0, 3354339
-li a1, 2133523020
-li a2, 615164656
-li a3, 195678923
-li a4, -1503798579
-li a5, -1507777128
-li a6, -1414844560
-li a7, -877408631
-or s5, a6, a1
-xor s1, a5, a0
-or s1, a5, a1
-mul s9, a2, a7
-sub s10, a5, a1
-add s5, a2, a5
-and s2, a0, a3
-and s5, a6, a4
-and s1, a5, a0
-and s10, a0, a2
-add s6, a7, a1
-or s7, a3, a1
-add s2, a4, a4
-xor s11, a6, a7
-or s3, a3, a6
-mul s4, a1, a2
+li a0, 163996410
+li a1, -1140825139
+li a2, 275894294
+li a3, 437922492
+li a4, -1150064028
+li a5, -496791922
+li a6, 1928404109
+li a7, -202745154
+jal sp, . + 4
+beq a0, a0, . + 16
+xor s5, a1, a5
+mul s3, a6, a1
+mul s4, a6, a7
+or s2, a3, a4
+mul s8, a5, a2
+mul s4, a2, a7
+and s3, a0, a6
+and s11, a7, a2
+mul s7, a4, a6
+sub s11, a0, a5
+sub s0, a7, a6
+add s11, a7, a5
+add s2, a6, a1
+add s10, a5, a0
+or s0, a2, a7
+or s9, a3, a5
+add s1, a4, a0
+or s10, a4, a1
+and s5, a7, a7
+xor s11, a7, a5
+sub s5, a3, a0
+xor s2, a2, a3
+and s9, a0, a6
+sub s7, a2, a1
+add s6, a3, a1
+sub s3, a1, a4
+add s6, a1, a6
+mul s5, a6, a4
+add s8, a3, a0
+or s11, a2, a3
+and s9, a3, a4
+beq a0, a0, . + 12
+and s3, a1, a4
+and s6, a3, a6
+xor s10, a2, a4
+beq a0, a0, . + 16
+beq zero, a0, . + 16
+sub s1, a6, a4
+or s3, a1, a0
+beq zero, a0, . + 16
+add s10, a4, a3
+jalr ra, sp, 172
+xor s2, a5, a2
+mul s2, a5, a1
+beq zero, a0, . + 12
+xor s5, a1, a0
+and s5, a2, a4
+and s4, a4, a7
+add s1, a5, a5
+or s3, a7, a0
+beq a0, a0, . + 8
+or s3, a2, a2
+beq a0, a0, . + 4
+or s1, a1, a7
+add s6, a2, a4
+xor s11, a5, a2
+jal ra, . + 12
+add s7, a4, a4
+beq a0, a0, . + 16
+beq zero, a0, . + 24
+mul s6, a3, a6
+jal ra, . + 8
+xor s1, a7, a0
+or s1, a4, a2
+add s4, a5, a4
+sub s3, a1, a4
+sub s4, a3, a7
+xor s8, a5, a6
+and s6, a1, a3
+add s2, a5, a2
+add s1, a4, a7
+mul s7, a2, a0
+or s6, a0, a7
+mul s0, a7, a5
+or s11, a4, a4
+and s0, a5, a3
+and s9, a7, a2
+xor s6, a5, a7
+beq a0, a0, . + 20
+sub s1, a0, a0
+jal ra, . + 24
+add s11, a2, a7
+beq zero, a0, . + 12
+xor s0, a6, a7
+or s0, a2, a1
+beq a0, a0, . + 8
+or s9, a5, a1
+mul s10, a5, a4
+mul s9, a3, a3
+mul s7, a3, a7
+mul s10, a6, a0
+or s2, a4, a2
+sub s4, a3, a3
+and s10, a4, a7
+jalr ra, sp, 392
+add s0, a7, a7
+add s0, a7, a5
+and s3, a1, a5
+xor s7, a6, a4
 nop
 nop
 nop
@@ -29,8 +114,12 @@ nop
 nop
 nop
 nop
-end:
-jal zero, end
+nop
+nop
+nop
+jal zero, .
+nop
+nop
 nop
 nop
 nop
--- Scripts/gen_random_prog.py
+++ Scripts/gen_random_prog.py
@@ -3,10 +3,40 @@ from random import randint, choice
 
 INT32_MIN = -2**31
 INT32_MAX = 2**31 - 1
+RAND_SECTION_LENGTH = 100
+CHANCE_OF_NON_BRANCH_INSTR = 80
+CHANCE_OF_COND_JMP = 80 #verses non conditional branch
+CHANCE_OF_BRANCH_TAKEN = 50
+CHANCE_OF_JAL_VS_JALR = 50
+
+# !CHANCE_OF_NON_BRANCH_INSTR
+#     CHANCE_OF_COND_JMP
+#     *   CHANCE_OF_BRANCH_TAKEN
+#     *   ***               
+#     *   !CHANCE_OF_BRANCH_TAKEN
+#     ***
+#     !CHANCE_OF_COND_JMP
+
+def gen_branch_section(instr:str, taken:bool, pc_offset:int) -> str:
+    if(instr == "beq"):
+        if(taken):
+            return f"beq a0, a0, . + {4*randint(1, 6)}\n"
+        else: #not taken
+            return f"beq zero, a0, . + {4*randint(1, 6)}\n"
+    
+    if(instr == "jal"):
+        return f"jal ra, . + {4*randint(1, 6)}\n"
+
+    if(instr == "jalr"):
+        return f"jalr ra, sp, {4*(pc_offset + randint(1, 6))}\n"
 
 def main():
 
-    instr = ["add", "sub", "mul", "and", "or", "xor"]
+    #pc_offset, excludes labels
+    pc_offset = 0
+
+    non_branch_instr = ["add", "sub", "mul", "and", "or", "xor"]
+    branch_instr = ["beq"]
     a_regs = ["a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7"]
     t_regs = ["t0", "t1", "t2", "t3", "t4", "t5", "t6"]
     s_regs = ["s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11"]
@@ -14,22 +44,57 @@ def main():
     with open("temp.s", "w") as f:
 
         for reg in a_regs:
-            f.write(
-                f"li {reg}, {randint(INT32_MIN, INT32_MAX)}\n"
-            )
+            f.write(f"li {reg}, {randint(INT32_MIN, INT32_MAX)}\n")
 
-        for ii in range(16):
-            f.write(
-                f"{choice(instr)} {choice(s_regs)}, {choice(a_regs)}, {choice(a_regs)}\n"
-            )
+        #genarate random instructions either branching or nonbranching
+        label_count = 0        
+        
+        #tags sp with the start of "rand_main:", this gets used as a refrence for the jalr
+        f.write(f"jal sp, . + 4\n")
+        #no pc_offset inc here
 
-        for jj in range(7):
-            f.write(
-                "nop\n"
-            )
+        for ii in range(RAND_SECTION_LENGTH):
 
-        f.write("end:\njal zero, end\n")
-        f.write("nop\nnop\nnop\n")
+            #non branch instructions
+            if(randint(0, 99) < CHANCE_OF_NON_BRANCH_INSTR):
+
+                f.write(f"{choice(non_branch_instr)} {choice(s_regs)}, {choice(a_regs)}, {choice(a_regs)}\n")
+                pc_offset = pc_offset + 1
+
+            else: #branch instructions
+                if(randint(0, 99) < CHANCE_OF_COND_JMP):
+                    if(randint(0, 99) < CHANCE_OF_BRANCH_TAKEN):
+
+                        f.write(gen_branch_section(choice(branch_instr), True, pc_offset))
+                        pc_offset = pc_offset + 1
+
+                    else:
+
+                        f.write(gen_branch_section(choice(branch_instr), False, pc_offset))
+                        pc_offset = pc_offset + 1
+
+                else:
+                    if(randint(0, 99) < CHANCE_OF_JAL_VS_JALR):
+
+                        f.write(gen_branch_section("jal", True, pc_offset))
+                        pc_offset = pc_offset + 1
+
+                    else:
+
+                        f.write(gen_branch_section("jalr", True, pc_offset))
+                        pc_offset = pc_offset + 1
+
+
+        #end of program
+
+        for jj in range(10):
+            f.write(f"nop\n")
+        f.write(f"jal zero, .\n")
+        f.write(f"nop\nnop\nnop\nnop\n")
+        pc_offset = pc_offset + 12
+        f.write(f"nop\n")
+        pc_offset = pc_offset + 1
+
 
     return 0
 
