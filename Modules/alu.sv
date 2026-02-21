23721a6ac9334d87b989e8133c939ef39fab2a08
ay <alaguda@asu.edu>
2026-02-21 13:21:28 -0700

    SRL, SLL, SRA, and their respective immediate instructions should shift by only the last 5 bits of the 32bit shift amount

--- Modules/alu.sv
+++ Modules/alu.sv
@@ -65,9 +65,9 @@ always_comb begin
     alu_sel_and : result = operand_a & operand_b; // AND
     alu_sel_or : result = operand_a | operand_b; // OR
     alu_sel_xor : result = operand_a ^ operand_b; // XOR
-    alu_sel_sll : result = operand_a << operand_b; // SLL
-    alu_sel_srl : result = operand_a >> operand_b; // SRL (zero-extension)
-    alu_sel_sra : result = signed_a >>> operand_b; // SRA (msb-extension)
+    alu_sel_sll : result = operand_a << operand_b[4:0]; // SLL
+    alu_sel_srl : result = operand_a >> operand_b[4:0]; // SRL (zero-extension)
+    alu_sel_sra : result = signed_a >>> operand_b[4:0]; // SRA (msb-extension)
     alu_sel_slt : result = {31'b0,slt_result}; // SLT
     alu_sel_sltu : result = {31'b0,sltu_result}; // SLTU
     default : result = '0;
