ec86934090823bef305f4505434a6a722bfda898
christoherleeCE <christopher.lee.engineer@outlook.com>
2026-02-25 20:26:52 -0700

    added misc scripts

---
+++ Scripts/grep_fail.sh
@@ -0,0 +1,5 @@
+grep -i fail *.log
+
+echo
+echo Finished...
+echo
---
+++ Scripts/tally.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+# Recursive globbing
+shopt -s globstar nullglob
+
+awk '
+BEGIN {
+    # RV32IM + selected pseudo instructions
+    split("\
+            add \
+            sub \
+            xor \
+            or \
+            and \
+            sll \
+            srl \
+            sra \
+            slt \
+            sltu \
+            addi \
+            xori \
+            ori \
+            andi \
+            slli \
+            srli \
+            srai \
+            slti \
+            sltiu \
+            lb \
+            lh \
+            lw \
+            lbu \
+            lhu \
+            sb \
+            sh \
+            sw \
+            beq \
+            bne \
+            blt \
+            bge \
+            bltu \
+            bgeu \
+            jal \
+            jalr \
+            lui \
+            auipc \
+            ecall \
+            ebreak \
+            mul \
+            mulh \
+            mulsu \
+            mulu \
+            div \
+            divu \
+            rem \
+            remu \
+            li \
+            nop",
+           insts)
+
+    for (i in insts) {
+        counts[insts[i]] = 0
+        valid[insts[i]] = 1
+    }
+}
+
+{
+    sub(/\r$/, "") #handles windows line endings or sumthin
+
+    # Remove comments
+    sub(/#.*/, "", $0)
+
+    # Trim leading whitespace
+    sub(/^[ \t]+/, "", $0)
+
+    # Skip empty lines
+    if ($0 == "") next
+
+    # Skip directives
+    if ($0 ~ /^\./) next
+
+    # Remove label if present
+    sub(/^[^:]+:[ \t]*/, "", $0)
+    if ($0 == "") next
+
+    # Extract first token (mnemonic)
+    split($0, fields, /[ \t,]+/)
+    inst = tolower(fields[1])
+
+    if (inst in valid) {
+        counts[inst]++
+    }
+}
+
+END {
+    print "==== Instruction Counts ===="
+    for (i in insts) {
+        printf "%-8s %d\n", insts[i], counts[insts[i]]
+    }
+}
+' **/*.s
