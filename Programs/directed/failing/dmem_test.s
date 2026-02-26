0e8cd6f48f08a0eebdd2888aa2477958e8f93006
christoherleeCE <christopher.lee.engineer@outlook.com>
2026-02-26 02:12:33 -0700

    reorganizing

---
+++ Programs/directed/failing/dmem_test.s
@@ -0,0 +1,74 @@
+    .section .text
+    .globl _start
+
+_start:
+    # Load address of word_array
+    la   t0, word_array
+
+    # Load first word
+    lw   t1, 0(t0)
+
+    # Load byte (signed)
+    lb   t2, 4(t0)
+
+    # Load halfword (unsigned)
+    lhu  t3, 6(t0)
+
+    # Store something into data section
+    la   t4, writable_word
+    sw   t1, 0(t4)
+
+    # Write to BSS
+    la   t5, buffer
+    sw   t3, 0(t5)
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    ebreak
+
+    ########################################################
+    # Read-only data section
+    ########################################################
+    .section .rodata
+const_string:
+    .asciz "Hello DMEM"
+
+const_table:
+    .word 0xDEADBEEF
+    .word 0xCAFEBABE
+    .word 0x12345678
+
+
+    ########################################################
+    # Initialized data section
+    ########################################################
+    .section .data
+
+# Word-aligned array
+    .align 2
+word_array:
+    .word 0x11223344
+    .byte 0x55
+    .byte 0x66
+    .half 0x7788
+    .word 13
+    .word -1
+
+writable_word:
+    .word 0xAABBCCDD
+
+
+    ########################################################
+    # Uninitialized data section
+    ########################################################
+    .section .bss
+
+    .align 2
+buffer:
+    .space 64
+
+counter:
+    .space 4
--- Scripts/load_binary_test.sh
+++ Scripts/load_binary_test.sh
@@ -3,10 +3,12 @@ set -e
 
 #riscv64-unknown-elf-as -march=rv32im program_asm.s -o program_asm.o
 
-riscv64-unknown-elf-gcc \
-  -march=rv32im -mabi=ilp32 \
-  -nostdlib -ffreestanding \
-  -c program_gcc_test.c -o program_asm.o
+# riscv64-unknown-elf-gcc \
+#   -march=rv32im -mabi=ilp32 \
+#   -nostdlib -ffreestanding \
+#   -c program_gcc_test.c -o program_asm.o
+
+riscv64-unknown-elf-as -march=rv32im "program_asm.s" -o program_asm.o
 
 riscv64-unknown-elf-ld \
   -m elf32lriscv \
@@ -16,7 +18,22 @@ riscv64-unknown-elf-ld \
 
 riscv64-unknown-elf-objcopy -O binary -j .text program_asm.elf instr.bin
 
-riscv64-unknown-elf-objcopy -O binary \
-  -j .data \
-  -j .rodata \
+# riscv64-unknown-elf-objcopy -O binary -j .text program_asm.elf instr.bin
+# hexdump -v -e '1/4 "%08x\n"' instr.bin > instruction_memory.txt
+
+# riscv64-unknown-elf-objcopy -O binary \
+#   -j .data \
+#   -j .rodata \
+#   program_asm.elf data.bin
+
+riscv64-unknown-elf-objcopy \
+  -O binary \
+  --only-section=.rodata \
+  --only-section=.data \
+  --only-section=.bss \
+  --change-section-lma .rodata=0 \
+  --change-section-lma .data=0 \
+  --change-section-lma .bss=0 \
+  --gap-fill 0x00 \
+  --pad-to 0x1000 \
   program_asm.elf data.bin
