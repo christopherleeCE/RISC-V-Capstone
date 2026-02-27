4cbf34bca51238869fa3a04b64c5fdce2602263d
egastelumm <egastelum0375@gmail.com>
2026-02-26 21:12:00 -0700

    Quick regression testing using random instruction generator. Enabled generation of division instructions. Added two blank assembly files for division/branching, will add/test programs sometime soon.

---
+++ Programs/directed/division.s
@@ -0,0 +1,11 @@
+# DIVISION TEST - Edgar G.
+# This program will test the division and modulo operations for both signed
+# and unsigned values (DIV, DIVU, REM, REMU).
+
+.global _start
+
+.text
+_start:
+    nop
+    nop
+    ebreak
