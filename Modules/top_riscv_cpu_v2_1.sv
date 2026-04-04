3e8f68eb000af61d4d288979d018c12760c4f4ba
christoherleeCE <christopher.lee.engineer@outlook.com>
2026-04-04 00:13:12 -0700

    doesnt matter, doesnt show up in the questa report so idc

--- Modules/top_riscv_cpu_v2_1.sv
+++ Modules/top_riscv_cpu_v2_1.sv
@@ -19,7 +19,6 @@ holden TODO bump up mem capacies cus why not we got like 180kb or sumthin like t
 TODO code coverage
 TODO more demo programs in C
 TODO contact abrams, maybe invite him to see the demo on demo day
-TODO try to fix dual port warning
 TODO maybe some cool external libs, like malloc
 
 
