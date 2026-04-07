552644d5cbe7fe9d243ca83073e9efe245b31d74
egastelumm <egastelum0375@gmail.com>
2026-04-06 20:21:04 -0700

    Added a simple program to solve a system of linear equations. Works okay, but open to suggestions/help on how to display the output (1D array of floats) properly on the FPGA.

---
+++ Programs/directed/functional_c/linear_system.c
@@ -0,0 +1,43 @@
+/* 
+
+FLOATING-POINT VERSION - 
+
+This program will solve a linear system of equations using Gaussian elimination
+and back substituion. Partial pivoting is used to reduce rounding errors. Not as
+fast as a dedicated linalg library, but mainly to test processor functionality.
+
+-Edgar G.
+
+*/
+
+//libraries
+#include "tb.h"
+#include "drysoup.h"
+#include "linalg_float.h"
+
+#define SCALE 1000000
+#define N 3 //width and height of matrices
+
+int main()
+{
+    float a_matrix[N][N] = 
+    {{1, -1, 1},
+     {2, 3, -1},
+     {3, -2, -9}};
+
+    float b_matrix[N] =
+    {8, -2, 9};
+
+    solve(N, a_matrix, b_matrix);
+
+    #ifdef x86_BUILD
+    printf("Solutions:\n");
+    for(int i = 0; i <= N-1; i++)
+        printf("%f ", b_matrix[i]);
+    printf("\n");
+    #endif
+
+    //return
+    return(tb_return(b_matrix[0]*SCALE));
+}
+
---
+++ Programs/directed/lib/linalg_float.c
@@ -0,0 +1,69 @@
+//function to solve the linear system
+#include "linalg_float.h"
+
+void solve(int n, float a[n][n], float b[n])
+{
+    gaussian_elim(n, a, b); //overwrites lower triangle w/ multipliers
+    back_sub(n, a, b); //solve w/ substitution, overwrite b w/ solution
+}
+
+//subroutine for gaussian elimination w/partial pivoting
+//this helps reduce rounding errors
+void gaussian_elim(int n, float a[n][n], float b[n])
+{
+    for (int k = 0; k < n-1; k++)
+    {
+        //PARTIAL PIVOTING
+        float temp_val = 0; //temporary value
+        float max_val = 0; //maximum value for partial pivoting
+        int max_in = 0; //index of maximum value
+
+        //search for the row with the largest pivot absolute value
+        for (int i = k; i < n; i++)
+            if ((temp_val = a[i][k]) > max_val || temp_val < -max_val)
+            {
+                max_in = i; //update previous values
+                max_val = temp_val;
+            }
+
+        //swap rows so that the pivot value has the largest value
+        //apply to both A and b
+        if (max_in > k)
+            for (int j = k; j < n; j++)
+            {
+                temp_val = a[k][j];
+                a[k][j] = a[max_in][j];
+                a[max_in][j] = temp_val;
+
+                temp_val = b[k];
+                b[k] = b[max_in];
+                b[max_in] = temp_val;
+            }
+
+        //GAUSSIAN ELIMINATION
+        //
+        for (int i = k+1; i < n; i++) //iterate through each row
+        {
+            a[i][k] = a[i][k]/a[k][k]; //pivot column in A
+
+            for (int j = k+1; j < n; j++) //iterate through remaining columns
+                a[i][j] -= a[i][k]*a[k][j];
+
+            b[i] -= a[i][k]*b[k]; //row operation on b 
+        }
+    }
+}
+
+void back_sub(int n, float a[n][n], float b[n])
+{
+    float temp_val = 0;
+
+    b[n-1] /= a[n-1][n-1];
+    for (int i = n-2; i >= 0; i--)
+    {
+        temp_val = 0; //reset value
+        for(int j = i+1; j < n; j++)
+            temp_val += a[i][j]*b[j]; //summation
+        b[i] = (b[i] - temp_val)/a[i][i];
+    }
+}
