0c115986bd025577e15afb1689b7a48acd836390
christopherleeCE <christopher.lee.engineer@outlook.com>
2026-04-09 14:17:37 -0700

    optimized the calloc

--- Programs/directed/functional_c/junk.c
+++ Programs/directed/functional_c/junk.c
@@ -24,21 +24,27 @@ int main(){
     //     };
     // arr[7] = num * 1000;
     //int* empty = malloc(4);
-    int* temp1 = malloc(0);
-    int* temp2 = malloc(4);
-    int* temp3 = malloc(8);
-    int* temp4 = malloc(0);
-    int* temp5 = realloc(NULL, 16);
+    int* ret0 = calloc(0, 1);
+    int* ret1 = calloc(1, 1);
+    int* ret2 = calloc(2, 1);
+    int* ret3 = calloc(3, 1);
+    int* ret4 = calloc(4, 1);
+    int* ret5 = calloc(5, 1);
+    int* ret6 = calloc(6, 1);
+    int* ret7 = calloc(7, 1);
+    int* ret8 = calloc(8, 1);
+    int* ret9 = calloc(9, 1);
 
-    free(temp1);
-    realloc(temp2, 0);
-    realloc(temp3, 1);
-    free(temp3);
-    temp5 = realloc(temp5, 20);
-    free(temp5);
-    free(temp4);
-
-    int* ret = calloc(40, 1);
+    free(ret1);
+    free(ret3);
+    free(ret4);
+    free(ret5);
+    free(ret6);
+    free(ret7);
+    free(ret8);
+    free(ret9);
+    free(ret2);
+    free(ret0);
 
     return tb_return(0);
 
