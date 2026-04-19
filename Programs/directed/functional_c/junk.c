8fd97ba558761e272dbe928d68cbdff3a28e4b11
christoherleeCE <christopher.lee.engineer@outlook.com>
2026-04-19 12:25:33 -0700

    n/a

--- Programs/directed/functional_c/junk.c
+++ Programs/directed/functional_c/junk.c
@@ -1,32 +1,11 @@
 #include "base.h"
-#include "drysoup.h"
-
-//volatile int num = 3;
 
 int main(){
 
-    int* ret0 = calloc(0, 1);
-    int* ret1 = calloc(1, 1);
-    int* ret2 = calloc(2, 1);
-    int* ret3 = calloc(3, 1);
-    int* ret4 = calloc(4, 1);
-    int* ret5 = calloc(5, 1);
-    int* ret6 = calloc(6, 1);
-    int* ret7 = calloc(7, 1);
-    int* ret8 = calloc(8, 1);
-    int* ret9 = calloc(9, 1);
+    int x = 5;
+    int y = 2;
+    int z = x + 2*y;
 
-    free(ret1);
-    free(ret3);
-    free(ret4);
-    free(ret5);
-    free(ret6);
-    free(ret7);
-    free(ret8);
-    free(ret9);
-    free(ret2);
-    free(ret0);
-
-    return tb_return(0, 0);
+    return tb_return(z, z);
 
 }
